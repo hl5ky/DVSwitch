6e0974e5b42dcfc7fc8c2f510dc93642097f9760
hl5ky <64560125+hl5ky@users.noreply.github.com>
2020-05-21 21:19:06 +0900

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DVS="/usr/local/dvs/"
+tmp="usr/local/dvs/tmp/"
+AB="/opt/Analog_Bridge/"
+MB="/opt/MMDVM_Bridge/"
+
+
+sudo wget -O ${tmp}txt.txt https://raw.githubusercontent.com/hl5ky/test/master/txt.txt
+sudo cp ${tmp}txt.txt ${AB}
