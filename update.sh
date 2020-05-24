2c363e8d9609fa33649aeb925ed72849ca880e89
hl5ky <64560125+hl5ky@users.noreply.github.com>
2020-05-24 18:50:51 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,6 +5,11 @@ tmp="/usr/local/dvs/tmp/"
 AB="/opt/Analog_Bridge/"
 MB="/opt/MMDVM_Bridge/"
 
+# add line
+#echo "abc" >> test.txt
+#echo "abc" | sudo tee -a test.txt
 
-sudo wget -O ${tmp}txt.txt https://raw.githubusercontent.com/hl5ky/test/master/txt.txt
-sudo cp ${tmp}txt.txt ${AB}
+
+
+#sudo wget -O ${tmp}txt.txt https://raw.githubusercontent.com/hl5ky/test/master/txt.txt
+#sudo cp ${tmp}txt.txt ${AB}
