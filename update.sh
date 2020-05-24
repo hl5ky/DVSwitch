e0403ad47fd26fb1316f5db0eaa36f341037cd32
hl5ky <64560125+hl5ky@users.noreply.github.com>
2020-05-24 22:48:50 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,6 +5,9 @@ tmp="/usr/local/dvs/tmp/"
 AB="/opt/Analog_Bridge/"
 MB="/opt/MMDVM_Bridge/"
 
+source ${DVS}lan/korean.txt
+source ${DVS}var.txt
+
 # add line
 #echo "abc" >> test.txt
 #echo "abc" | sudo tee -a test.txt
@@ -13,3 +16,11 @@ MB="/opt/MMDVM_Bridge/"
 
 #sudo wget -O ${tmp}txt.txt https://raw.githubusercontent.com/hl5ky/test/master/txt.txt
 #sudo cp ${tmp}txt.txt ${AB}
+
+# 200523 New update of macro
+if [ ${date_update_scripts} -lt 200523 ]; then
+
+sudo sed -i -e "/^date_update_scripts=/ c date_update_scripts=200523" "${DVS}lan/korean.txt"
+fi
+
+
