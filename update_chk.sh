a3eae5ffac3bcd79c6fd8948863ab6a1eafa5cdb
hl5ky <64560125+hl5ky@users.noreply.github.com>
2020-05-27 20:17:39 +0900

    Create update_chk.sh

---
+++ update_chk.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DVS="/usr/local/dvs/"
+tmp="/usr/local/dvs/tmp/"
+
+source ${DVS}lan/korean.txt
+source ${DVS}var.txt
+
+new_date=200523
+
+if [ ${date_update_scripts} -lt ${new_date} ];
+then
+        if (whiptail --title " 업데이트 " --yesno "\
+\n
+                    새로운 업데이트가 있습니다.
+                      업데이트 하시겠습니까?
+" 12 70); then
+        sudo sed -i -e "/^date_update_scripts=/ c date_update_scripts=${new_date}" "${DVS}lan/korean.txt"
+        sudo wget -O ${DVS}update.sh https://raw.githubusercontent.com/hl5ky/DVSwitch/master/update.sh > /dev/null 2>&1
+        sudo chmod +x ${DVS}update.sh > /dev/null 2>&1
+        sudo ${DVS}update.sh
+        else clear; ${DVS}tools_menu.sh; exit 0
+        fi
+else
+whiptail --msgbox "\
+\n
+                   새로운 업데이트가 없습니다.
+" 10 70 1
+sudo rm ${DVS}update_chk.sh
+fi
+
+
