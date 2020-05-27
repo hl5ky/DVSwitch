7daa0c73778a9c10bbded93c2b402b60d9da427b
hl5ky <64560125+hl5ky@users.noreply.github.com>
2020-05-27 16:46:49 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,11 +8,7 @@ MB="/opt/MMDVM_Bridge/"
 source ${DVS}lan/korean.txt
 source ${DVS}var.txt
 
-new_date=200525
-if [ ${date_update_scripts} -lt ${new_date} ]; then  새로운 버전 있음
-else 없음
-fi
-
+new_date=200527
 
 
 # add line
@@ -24,10 +20,43 @@ fi
 #sudo wget -O ${tmp}txt.txt https://raw.githubusercontent.com/hl5ky/test/master/txt.txt
 #sudo cp ${tmp}txt.txt ${AB}
 
-# 200523 New update of macro
-if [ ${date_update_scripts} -lt 200523 ]; then
 
-sudo sed -i -e "/^date_update_scripts=/ c date_update_scripts=200523" "${DVS}lan/korean.txt"
+
+function update() {
+
+
+echo "업데이트 실행"
+
+sudo rm ${DVS}test.txt > /dev/null 2>&1
+
+
+}
+
+#-----------------------------------------------------------------------------------
+
+if [ ${date_update_scripts} -lt ${new_date} ];
+then
+        if (whiptail --title " 업데이트 " --yesno "\
+\n
+                    새로운 업데이트가 있습니다.
+
+                      업데이트 하시겠습니까?
+
+" 12 70); then
+        sudo sed -i -e "/^date_update_scripts=/ c date_update_scripts=${new_date}" "${DVS}lan/korean.txt"
+        update
+        else clear; ${DVS}update_upgrade.sh; exit 0
+        fi
+else
+whiptail --msgbox "\
+\n
+                    새로운 업데이트가 없습니다.
+
+" 10 70 1
+
+sudo rm ${DVS}test.txt > /dev/null 2>&1
 fi
 
+#clear; ${DVS}update_upgrade.sh; exit 0
 
+exit 0
